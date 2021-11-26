53fb49d30f2bd11a9d65237d88092e5a906f1c9a
Brooks_Wilcox <brookswill88@gmail.com>
2021-11-26 16:24:21 -0700

    react hooks jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
 
+
+
 function About() {
   return (
-    <div id="About">
+    <div id="about">
       <h2 style={{textAlign: "center"}}>About Me</h2>
       <p style={{textAlign: "center"}}>Look at me NOW</p>
-      <img src={image}></img>
+      <img src={image} alt="I made this" />
     </div>
   )
 }
